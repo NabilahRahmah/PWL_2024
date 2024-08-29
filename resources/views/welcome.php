18fa771499707aaad0dc69e3f579b799c9fb142b
NabilahRahmah <nabilah.oppo13@gmail.com>
2024-08-29 18:35:54 +0700

    perubahan tugas

---
+++ resources/views/welcome.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Kartu Tanda Penduduk (KTP) Indonesia</title>
+<script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body class="bg-gray-200 font-sans">
+
+<div class="ktp w-full max-w-2xl bg-blue-500 text-white shadow-md rounded-lg overflow-hidden mx-auto mt-12">
+    <div class="ktp-header text-center py-2">
+        <h1 class="text-lg font-bold uppercase text-black">Provinsi Jawa Timur <br>Kabupaten Sidoarjo</h1>
+    </div>
+
+    <div class="ktp-info p-5 font-bold text-black">
+        <div class="ktp-info-item flex mb-1">
+            <span class="ktp-info-label w-36">NIK</span>
+            <span>: 35151xxxxxxxxx</span>
+        </div>
+        <div class="ktp-info-item flex mb-1">
+            <span class="ktp-info-label w-36">Nama</span>
+            <span>: NABILAH RAHMAH D</span>
+        </div>
+        <div class="ktp-info-item flex mb-1">
+            <span class="ktp-info-label w-36">Tempat / Tgl Lahir</span>
+            <span>: SIDOARJO, 1*-1*-2***</span>
+        </div>
+        <div class="ktp-info-item flex mb-1">
+            <span class="ktp-info-label w-36">Jenis Kelamin</span>
+            <span>: Perempuan</span>
+            <span class="ktp-info-label ml-12">Gol Darah</span>
+            <span>: A</span>
+        </div>
+        <div class="ktp-info-item flex mb-1">
+            <span class="ktp-info-label w-36">Alamat</span>
+            <span>: DSN.KEMAMBANG</span>
+        </div>
+        <div class="ktp-info-item flex mb-1 ml-6">
+            <span class="ktp-info-label w-36">RT / RW</span>
+            <span>: 0** / 0**</span>
+        </div>
+        <div class="ktp-info-item flex mb-1 ml-6">
+            <span class="ktp-info-label w-36">Kelurahan</span>
+            <span>: SIDOARJO</span>
+        </div>
+        <div class="ktp-info-item flex mb-1 ml-6">
+            <span class="ktp-info-label w-36">Kecamatan</span>
+            <span>: BALONGBENDO</span>
+        </div>
+        <div class="ktp-info-item flex mb-1">
+            <span class="ktp-info-label w-36">Agama</span>
+            <span>: Islam</span>
+        </div>
+        <div class="ktp-info-item flex mb-1">
+            <span class="ktp-info-label w-36">Status Perkawinan</span>
+            <span>: Belum Kawin</span>
+        </div>
+        <div class="ktp-info-item flex mb-1">
+            <span class="ktp-info-label w-36">Pekerjaan</span>
+            <span>: Pelajar / Mahasiswa</span>
+        </div>
+        <div class="ktp-info-item flex mb-1">
+            <span class="ktp-info-label w-36">Kewarganegaraan</span>
+            <span>: WNI</span>
+        </div>
+        <div class="ktp-info-item flex mb-1">
+            <span class="ktp-info-label w-36">Berlaku Hingga</span>
+            <span>: SEUMUR HIDUP</span>
+        </div>
+    </div>
+</div>
+</body>
+</html>
