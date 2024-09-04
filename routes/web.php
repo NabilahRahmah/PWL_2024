af6ce7d5c1a7c1ef381d9c16dffc3c9def89b783
NabilahRahmah <nabilah.oppo13@gmail.com>
2024-09-04 10:30:28 +0700

    Routing

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,41 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'welcome1';
+});
+
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+Route::get('/world', function () {
+    return 'World';
+});
+
+Route::get('/', function () {
+    return 'Selamat Datang';
+});
+
+Route::get('/about', function () {
+    return 'NIM: 2241760013<br>Nama: Nabilah Rahmah';
+});
+
+Route::get('/user/{name}', function ($name) {
+    return "Nabilah Rahmah" . $name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+return 'Pos ke-13'.$postId." Komentar ke-3".$commentId;
+});
+
+Route::get( '/articles/{id}', function ($id) {
+    echo "Halaman Artikel dengan ID 13" . $id;
+});
+
+Route::get('/user/{name?}', function ($name = null) {
+    return "Nabilah Rahmah" . $name;
+});
+
+Route::get('/user/{name?}', function ($name='John') {
+    return "Nabilah Rahmah" . $name;
 });
