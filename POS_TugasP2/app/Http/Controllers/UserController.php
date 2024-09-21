7efcd154d7255c422a84ab61d7c4f004c57eb312
NabilahRahmah <nabilah.oppo13@gmail.com>
2024-09-21 23:31:43 +0700

    js4 prak1.2

--- POS_TugasP2/app/Http/Controllers/UserController.php
+++ POS_TugasP2/app/Http/Controllers/UserController.php
@@ -48,15 +48,22 @@ class UserController extends Controller
     //     return view( 'user', ['data' => $user]);
 
      //Jobsheet 4 Prak 1.2
-    $data
-        = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345')
-        ];
-    UserModel::create($data);
-    $user = UserModel::all();
-    return view('user', ['data' => $user]);
+    // $data
+    //     = [
+    //         'level_id' => 2,
+    //         'username' => 'manager_tiga',
+    //         'nama' => 'Manager 3',
+    //         'password' => Hash::make('12345')
+    //     ];
+    // UserModel::create($data);
+    // $user = UserModel::all();
+    // return view('user', ['data' => $user]);
+
+    //Js4 Prak 2.1
+        $user = UserModel::findOr(1, ['username','nama'], function (){
+            abort(404);
+        });
+        return view('user', ['data' => $user]);
+
     }
 }
